8831549129147edc03edd2afdc270f1e35bda3ba
leojavier5 <leandro.javier.perez.gonzalez@gmail.com>
2023-10-13 16:34:51 -0300

    avance-1 front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Carrusel from "./components/Carrusel";
+import { Route, Routes, Link } from "react-router-dom";
+import Calendario from "./Pages/Calendario";
+import Home from "./Pages/Home";
+import Controles from "./Pages/Controles";
+import Medicamentos from "./Pages/Medicamentos";
+import Tips from "./Pages/Tips";
 
 function App() {
+  const config = {
+    titulo: "Kumelsalud",
+    autor: "grupo4",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendario" element={<Calendario />} />
+          <Route path="/controles" element={<Controles />} />
+          <Route path="/medicamentos" element={<Medicamentos />} />
+          <Route path="/tips" element={<Tips />} />
+        </Routes>
+      </Main>
+      <Footer autor={config.autor} />
+    </>
   );
 }
 
---
+++ src/Pages/Controles.jsx
@@ -0,0 +1,3 @@
+export default function Controles() {
+  return <h1>controles</h1>;
+}
